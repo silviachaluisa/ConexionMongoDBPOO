8632e8cc940b1350983f21be63b8d6db325dda14
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-29 13:58:19 -0500

    Creacion ventana

---
+++ src/main/java/org/example/ConexionMDB.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class ConexionMDB {
+    private JTextField textField3;
+    private JTextField textField2;
+    private JTextField textField4;
+    private JButton insertarButton;
+    private JButton eliminarButton;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JComboBox servidor;
+}
